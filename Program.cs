30830952f42d136a4ea60696d49b7b94920b806d
Konstantine <kach91kach91@gmail.com>
2024-03-06 17:36:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_to_delete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
